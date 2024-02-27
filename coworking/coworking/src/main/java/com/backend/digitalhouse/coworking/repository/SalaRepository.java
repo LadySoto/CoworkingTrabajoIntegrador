a5b37af2ac47e2fa4a306f2219a504ba8a259071
KateLLano <kate2811llano@gmail.com>
2024-02-27 12:41:12 -0500

    agrego service template

---
+++ coworking/coworking/src/main/java/com/backend/digitalhouse/coworking/repository/SalaRepository.java
@@ -0,0 +1,2 @@
+package com.backend.digitalhouse.coworking.repository;public interface SalaRepository {
+}
---
+++ coworking/coworking/src/main/java/com/backend/digitalhouse/coworking/service/implement/RolService.java
@@ -0,0 +1,2 @@
+package com.backend.digitalhouse.coworking.service.impl;public class RolService {
+}
