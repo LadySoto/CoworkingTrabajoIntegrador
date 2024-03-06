2c2c037bd2cba7807cfa26adce24337cc0aac6c3
Diana V. R <dpvarin@gmail.com>
2024-03-06 09:02:17 -0500

    Desarrollando JWT

--- coworking/coworking/src/main/java/com/backend/digitalhouse/coworking/service/JwtService.java
+++ coworking/coworking/src/main/java/com/backend/digitalhouse/coworking/service/JwtService.java
@@ -1,29 +1,45 @@
 package com.backend.digitalhouse.coworking.service;
 
+import com.backend.digitalhouse.coworking.CoworkingApplication;
 import com.backend.digitalhouse.coworking.entity.Usuario;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
 @Service
 public class JwtService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtService.class);
     @Value("${security.jwt.expiration-minutes}")
     private long EXPIRATION_MINUTES;
-       public String generateToken(Usuario usuario, Map<String, Object> extraClaims) {
-           Date issuedAt = new Date(System.currentTimeMillis());
-           Date expiration = new Date(issuedAt.getTime() + (EXPIRATION_MINUTES * 60 * 1000));
 
-           Jwts.builder()
-                   .setClaims(extraClaims)
-                   .setSubject(usuario.getNombre())
-                   .setIssuedAt(issuedAt)
-                   .setExpiration(expiration)
-                   .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                   .signWith(generateKey(),SignatureAlgoritm.HS256);
+    @Value("${security.jwt.secret-key}")
+    private String SECRET_KEY;
+    public String generateToken(Usuario usuario, Map<String, Object> extraClaims) {
+        Date issuedAt = new Date(System.currentTimeMillis());
+        Date expiration = new Date(issuedAt.getTime() + (EXPIRATION_MINUTES * 60 * 1000));
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(usuario.getNombre())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();
+      }
+    private Key generateKey(){
+        byte [] secretAsBytes = Decoders.BASE64.decode(SECRET_KEY);
+        LOGGER.info("mi clave es: {}", new String(secretAsBytes));
+           return Keys.hmacShaKeyFor(secretAsBytes);
     }
 }
