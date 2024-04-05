f231ab32fda7aeb1e9f47137244d973448a9ac81
KateLLano <kate2811llano@gmail.com>
2024-04-05 00:34:29 -0500

    Se corrigen errores y se agrega validacion para que no hayan servicios por el mismo nombre

--- coworking/coworking/src/main/java/com/backend/digitalhouse/coworking/repository/ServicioRepository.java
+++ coworking/coworking/src/main/java/com/backend/digitalhouse/coworking/repository/ServicioRepository.java
@@ -1,9 +1,13 @@
 package com.backend.digitalhouse.coworking.repository;
 
+import com.backend.digitalhouse.coworking.entity.Sala;
 import com.backend.digitalhouse.coworking.entity.Servicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServicioRepository extends JpaRepository<Servicio, Long> {
+    List<Servicio> findByNombreContaining(String nombre);
 }
